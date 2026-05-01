d7e17193c1e4031a37a80246fb8b313acb8156ae
Paul Coroneos <paul.coroneos@icloud.com>
2026-05-01 17:53:41 -0500

    1-4

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,20 +1,29 @@
 import argparse
+import json
 
 def main() -> None:
-    parser = argparse.ArgumentParser(description="Keyword Search CLI")
-    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+  parser = argparse.ArgumentParser(description="Keyword Search CLI")
+  subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
-    search_parser = subparsers.add_parser("search", help="Search movies using BM25")
-    search_parser.add_argument("query", type=str, help="Search query")
+  search_parser = subparsers.add_parser("search", help="Search movies using BM25")
+  search_parser.add_argument("query", type=str, help="Search query")
 
-    args = parser.parse_args()
+  args = parser.parse_args()
 
-    match args.command:
-        case "search":
-            print(f"Searching for: {args.query}")
-            pass
-        case _:
-            parser.print_help()
+  match args.command:
+      case "search":
+        print(f"Searching for: {args.query}")
+        count = 1
+        with open('data/movies.json', 'r') as f:
+          data = json.load(f)
+          for movie in data['movies']:
+            if count > 5:
+              break
+            if args.query in movie["title"]:
+              print(f"{count}. {movie['title']}")
+              count = count + 1
+      case _:
+          parser.print_help()
 
 if __name__ == "__main__":
-    main()
+  main()
