c0c8f1304f4a54fc1df18707db138d403acc6677
PaulACoroneos <paul.coroneos@icloud.com>
2026-05-04 23:38:59 -0500

    1-9

--- utilities/text_utils.py
+++ utilities/text_utils.py
@@ -1,3 +1,6 @@
+from nltk.stem import PorterStemmer
+
+
 def strip_punctuation(string_with_punctuation):
     replace_function = str.maketrans(
         {",": "", ".": "", "!": "", ":": "", "'": "", "-": ""}
@@ -15,21 +18,29 @@ def remove_stopwords_from_phrase(phrase, stop_words):
     return " ".join(word for word in phrase.split() if word not in stop_words)
 
 
+def stem_phrase(phrase):
+    stemmer = PorterStemmer()
+    return " ".join(stemmer.stem(word) for word in phrase.split())
+
+
 def sanitize_query(query, stop_words):
     lowered_query = query.lower()
     stipped_punction_and_lowered = strip_punctuation(lowered_query)
-    sanitized_query = remove_stopwords_from_phrase(
+    stopwords_removed = remove_stopwords_from_phrase(
         stipped_punction_and_lowered, stop_words
     )
+    stemmed_query = stem_phrase(stopwords_removed)
 
-    return sanitized_query
+    return stemmed_query
 
 
 def sanitize_movie_titles(movies_data, stop_words):
     sanitized_movie_titles_with_original_titles = []
     for movie in movies_data["movies"]:
-        clean_title = remove_stopwords_from_phrase(
-            strip_punctuation(movie["title"].lower()), stop_words
+        clean_title = stem_phrase(
+            remove_stopwords_from_phrase(
+                strip_punctuation(movie["title"].lower()), stop_words
+            )
         )
         sanitized_movie_titles_with_original_titles.append(
             (movie["title"], clean_title)
