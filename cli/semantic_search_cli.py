333a45de391a1480bb9bac472a96233fba17f7b0
PaulACoroneos <paul.coroneos@icloud.com>
2026-05-19 16:25:50 -0500

    4-10

--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -2,19 +2,23 @@
 
 import argparse
 
-from cli.lib.semantic_search import SemanticSearch
+from cli.lib.semantic_search import SemanticSearch, embed_text
 
 
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
     subparsers.add_parser("verify", help="Verify the model")
+    embed_text_parser = subparsers.add_parser("embed_text", help="Embed text")
+    embed_text_parser.add_argument("text", help="Text to embed")
 
     args = parser.parse_args()
 
     semantic_search = SemanticSearch()
 
     match args.command:
+        case "embed_text":
+            embed_text(args.text)
         case "verify":
             semantic_search.verify_model()
         case _:
